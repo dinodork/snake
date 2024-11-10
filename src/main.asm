5927b752ed05c4344368893e11d79ed9b53d3552
Martin Hansson <martin.hansson@generalsystem.com>
2024-11-10 11:30:37 +0100

    Extracted printing of the Game Over message into a macro.

--- src/main.asm
+++ src/main.asm
@@ -26,6 +26,7 @@ screen_top: defb  0   ; WPMEMx
   include "src/keyboard.z80"
   include "messages.z80"
   include "message_strings.z80"
+  include "messages.z80"
 
   include "build/graphics/font.asm"
   include "build/graphics/frames.asm"
